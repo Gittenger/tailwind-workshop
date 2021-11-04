f29013d66d503007851e511133be9ee1debad58f
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-11-04 15:07:24 -0400

    testing IconLink component

--- src/pages/HomePageContent.component.jsx
+++ src/pages/HomePageContent.component.jsx
@@ -1,19 +1,21 @@
 import React, { useEffect } from 'react'
 
 import CIndex from '../components/components.index.js'
+import images from '../assets/img-index.js'
 
 const HomePageContent = () => {
-  const { Button, Login } = CIndex
+  const { Button, Login, IconLink } = CIndex
+  const {
+    svg: { Facebook, Instagram, Email, ChevronUp },
+  } = images
+
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
 
   return (
     <>
-      <Login />
-      <Button />
-      {/* <Button color="blue" /> */}
-      {/* <Button color="green" /> */}
+      <IconLink border="rounded" to="/page" background SvgComponent={Email} />
     </>
   )
 }
