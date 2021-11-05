5142f6658b9841febce33e17ee7148e255b0b830
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-11-05 13:13:00 -0400

    icons

--- src/assets/img-index.js
+++ src/assets/img-index.js
@@ -7,19 +7,31 @@ import { ReactComponent as Facebook } from './icons/facebook.svg'
 import { ReactComponent as Instagram } from './icons/instagram.svg'
 import { ReactComponent as Email } from './icons/email.svg'
 import { ReactComponent as ChevronUp } from './icons/chevron-up.svg'
+import Bluetooth from './tiny/bluetooth.png'
+import Firefox from './tiny/firefox.png'
+import Folder from './tiny/folder.svg'
+import Chrome from './tiny/google-chrome.png'
+import Vlc from './tiny/vlc.png'
 
 export const images = {
-	img: {
-		img1: ImgOne,
-		img2: ImgTwo,
-		img3: ImgTnree,
-	},
-	svg: {
-		Facebook,
-		Instagram,
-		Email,
-		ChevronUp,
-	},
+  img: {
+    img1: ImgOne,
+    img2: ImgTwo,
+    img3: ImgTnree,
+  },
+  svg: {
+    Facebook,
+    Instagram,
+    Email,
+    ChevronUp,
+  },
+  tiny: {
+    Bluetooth,
+    Firefox,
+    Folder,
+    Chrome,
+    Vlc,
+  },
 }
 
 export default images
