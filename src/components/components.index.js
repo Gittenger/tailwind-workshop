4fa23fe80ad92b2827b059ddcb137bfd93e8745b
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-06 19:06:11 -0500

    dl some comp

--- src/components/components.index.js
+++ src/components/components.index.js
@@ -1,29 +1,14 @@
-import Button from './General/Button.component.jsx'
-import Checkbox from './General/Checkbox.component.jsx'
-import ContactForm from './General/ContactForm.component.jsx'
-import ExpandableList from './General/ExpandableList.component.jsx'
-import FooterColumn from './General/FooterColumn.component.jsx'
-import Header from './General/Header.component.jsx'
-import HistoryItem from './General/HistoryItem.component.jsx'
-import IconLink from './General/IconLink.component.jsx'
-import IconListItem from './General/IconListItem.component.jsx'
-import Layout from './General/Layout.component.jsx'
-import LineItem from './General/LineItem.component.jsx'
-import Login from './General/Login.component.jsx'
-import VideoSkeleton from './General/VideoSkeleton.component.jsx'
+import Button from "./General/Button.component.jsx"
+import Checkbox from "./General/Checkbox.component.jsx"
+import ContactForm from "./General/ContactForm.component.jsx"
+import ExpandableList from "./General/ExpandableList.component.jsx"
+import Header from "./General/Header.component.jsx"
+import IconLink from "./General/IconLink.component.jsx"
+import IconListItem from "./General/IconListItem.component.jsx"
+import Layout from "./General/Layout.component.jsx"
+import Login from "./General/Login.component.jsx"
 export const CIndex = {
-  Button,
-  Checkbox,
-  ContactForm,
-  ExpandableList,
-  FooterColumn,
-  Header,
-  HistoryItem,
-  IconLink,
-  IconListItem,
-  Layout,
-  LineItem,
-  Login,
-  VideoSkeleton,
-}
-export default CIndex
+		Button,Checkbox,ContactForm,ExpandableList,Header,IconLink,IconListItem,Layout,Login
+	}
+	export default CIndex
+	
--- src/pages/HomePageContent.component.jsx
+++ src/pages/HomePageContent.component.jsx
@@ -4,46 +4,11 @@ import CIndex from '../components/components.index.js'
 import images from '../assets/img-index.js'
 
 const HomePageContent = () => {
-  const {
-    HistoryItem,
-    IconLink,
-    IconListItem,
-    FooterColumn,
-    LineItem,
-    VideoSkeleton,
-  } = CIndex
+  const { IconLink, IconListItem } = CIndex
   const {
     svg: { Pug, Git, ReactIcon, Facebook },
     tiny: { Bluetooth, Chrome, Firefox, Folder, Vlc },
   } = images
-
-  const testFooterLists = {
-    '1': {
-      title: 'Support',
-      list: [
-        { url: '/', text: 'Help' },
-        { url: '/', text: 'Community' },
-        { url: '/', text: 'Contact Us' },
-      ],
-    },
-    '2': {
-      title: 'Our Company',
-      list: [
-        { url: '/', text: 'About' },
-        { url: '/', text: 'Blog' },
-        { url: '/', text: 'Media' },
-      ],
-    },
-    '3': {
-      title: 'Terms & Policies',
-      list: [
-        { url: '/', text: 'Policies' },
-        { url: '/', text: 'Terms of Use' },
-        { url: '/', text: 'Code of Conduct' },
-        { url: '/', text: 'Privacy' },
-      ],
-    },
-  }
 
   useEffect(() => {
     window.scrollTo(0, 0)
@@ -51,46 +16,13 @@ const HomePageContent = () => {
 
   return (
     <>
-      <div className="w-full grid grid-cols-1 justify-items-center px-5">
-        {/* <HistoryItem /> */}
+      <div className="w-full grid justify-items-center px-5">
         {/* <IconListItem
           imgSrc={Bluetooth}
           textContent="Bluetooth Folder"
           orientation="right"
           countBubble="33"
         /> */}
-        <div className="w-full grid gap-4 grid-cols-1 sm:grid-cols-2 justify-items-center">
-          <VideoSkeleton />
-          <VideoSkeleton />
-          <VideoSkeleton />
-        </div>
-        <ul className="space-y-8 mt-10 w-full">
-          <li>
-            <FooterColumn listData={testFooterLists['1']} />
-          </li>
-          <li>
-            <FooterColumn listData={testFooterLists['2']} />
-          </li>
-          <li>
-            <FooterColumn listData={testFooterLists['3']} />
-          </li>
-        </ul>
-        {/* <LineItem
-            textContent="November 2021"
-            cost="$7.99"
-            status={{ text: 'needs attention', code: 'warn' }}
-            linkTo="/"
-          />
-          <LineItem
-            textContent="October 2021"
-            cost="$7.99"
-            status={{ text: 'pending', code: 'pending' }}
-          />
-          <LineItem
-            textContent="September 2021"
-            cost="$7.99"
-            status={{ text: 'paid', code: 'good' }}
-          /> */}
       </div>
     </>
   )
